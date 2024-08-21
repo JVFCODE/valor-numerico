56e3d5cb839019468753a487a0a3b56e445e1255
JVFCODE <164587661+JVFCODE@users.noreply.github.com>
2024-08-21 20:56:31 -0300

    Add files via upload

---
+++ Exemplo1.java
@@ -0,0 +1,19 @@
+package eu.com.br;
+
+public class Exemplo1 {
+	
+	public static void main(String args[]) {
+		
+		Boolean status = true;
+		Boolean status = Boolean.TRUE;
+		
+		Character c = 'A';
+		
+		
+		Integer idadade = 11;
+		
+		Long cpf = 12343l;
+		
+	}
+
+}
